0391874b666e0ff34133bc4c02642615ec915c5f
Olha Diakiv <olya.dyakiv26@gmail.com>
2025-01-01 17:30:39 +0100

    finish: 003-kaboom-2d-portfolio

---
+++ 003-kaboom-2d-portfolio/src/constants.js
@@ -0,0 +1,10 @@
+export const scaleFactor = 4
+
+export const dialogueData = {
+    pc: 'pc',
+    tv: 'tv',
+    bed: 'bed',
+    resume: 'resume',
+    projects: 'projects',
+    library: 'library'
+}
